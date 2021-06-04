6beb3b7a86efe7efc3a9c033b06bd4f8be4618f6
Cihan <cihangonen35@gmail.com>
2021-06-04 22:06:11 +0300

    courses page works

--- app.js
+++ app.js
@@ -1,25 +1,33 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const pageRoute = require('./routes/pageRoute');
+const courseRoute = require('./routes/courseRoute');
 
 const app = express();
+
+//Connect DB
+mongoose
+  .connect('mongodb://localhost/smartedu-db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log('connected to db succesfully');
+  });
 
 //Template Engine
 app.set('view engine', 'ejs');
 
 //Middlewares
 app.use(express.static('public'));
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 //Routes
-app.get('/', (req, res) => {
-  res.render('index', {
-    page_name: 'index',
-  });
-});
-
-app.get('/about', (req, res) => {
-  res.render('about', {
-    page_name: 'about',
-  });
-});
+app.use('/', pageRoute);
+app.use('/courses', courseRoute);
 
 const port = 3000;
 
