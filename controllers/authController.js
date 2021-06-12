af044611bfe0288ecf3ac82428be3cf6a538f5fb
Cihan <cihangonen35@gmail.com>
2021-06-12 21:53:56 +0300

    create course

---
+++ controllers/authController.js
@@ -0,0 +1,56 @@
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
+const Category = require('../models/Category');
+
+exports.createUser = async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      user,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
+    });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    await User.findOne({ email }, (err, user) => {
+      if (user) {
+        bcrypt.compare(password, user.password, (err, same) => {
+          if (same) {
+            // USER SESSION
+            req.session.userID = user._id;
+            res.status(200).redirect('/users/dashboard');
+          }
+        });
+      }
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
+    });
+  }
+};
+
+exports.logoutUser = (req, res) => {
+  req.session.destroy(() => {
+    res.redirect('/');
+  });
+};
+
+exports.getDashboardPage = async (req, res) => {
+  const user = await User.findOne({ _id: req.session.userID });
+  const categories = await Category.find();
+  res.render('dashboard', {
+    page_name: 'dashboard',
+    user,
+    categories,
+  });
+};
